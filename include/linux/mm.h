73e57921abafb5cff363bf03c7d20348dc97cf5a
Andrew Fasano <fasano@mit.edu>
2023-06-30 10:37:46 -0400

    Disable igloo_task_size log (it was happening a lot)

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -35,7 +35,6 @@ static int __init early_igloo_task_size(char *p) {
         return -1;
     }
     igloo_task_size = task_size;
-    pr_warn("Using igloo_task_size: 0x%lx\n", igloo_task_size);
     return 0;
 }
 early_param("igloo_task_size", early_igloo_task_size);
