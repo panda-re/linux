1688c7c668ab7fa632826e09c64d2a214d5b07af
Andrew Fasano <fasano@mit.edu>
2023-07-20 15:59:33 -0400

    setup/all: support mips64eb

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -eux
 
-# valid: arm, mipsel, mipseb
+# valid: arm, mipsel, mipseb, mips64eb.
+# Need to map to short_arch for make target
+# Need  config.${arch} file
+# and need /cross/${ARCH}... cross compiler
 ARCH=$1
 
 SHORT_ARCH=mips
@@ -22,6 +25,13 @@ if [ "$ARCH" = "armel" ] || [ "$ARCH" = "armeb" ]; then
 
   # Even for armeb, we use same compiler. Big endian just needs an extra cflag
   CROSS_CC=/cross/arm-linux-musleabi/bin/arm-linux-musleabi-
+elif [ "$ARCH" = "mips64eb" ] || [ "$ARCH" = "mips64el" ]; then
+  if [ "$ARCH" = "mips64el" ]; then
+    echo "mips64el unsupported"
+    exit 1
+  fi
+  SHORT_ARCH=mips
+  CROSS_CC=/opt/cross/mips64-linux-musl/bin/mips64-linux-musl-
 else
   CROSS_CC=/cross/${ARCH}-linux-musl${ABI}/bin/${ARCH}-linux-musl${ABI}-
 fi
@@ -37,3 +47,15 @@ fi
 
 echo "Building kernel"
 make ARCH=${SHORT_ARCH} CROSS_COMPILE=${CROSS_CC} O=build/${ARCH} $TARGETS -j$(nproc)
+
+#PANDA=~/git/panda
+#echo 'Updating PANDA info'
+#${PANDA}/panda/plugins/osi_linux/utils/kernelinfo_gdb/run.sh ./build/${ARCH}/vmlinux ./panda_profile.${ARCH}
+
+#echo 'Building volatility profile'
+#dwarf2json wants a newer go than ships with Ubuntu 20.04, so I did this:
+#mkdir -p ~/go-1.14.2
+#wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | tar -xz -C ~/go-1.14.2
+#~/go-1.14.2/go/bin/go
+#DWARF2JSON=~/git/dwarf2json/dwarf2json
+#${DWARF2JSON} linux --elf build/${ARCH}/vmlinux | xz - > ./vmlinux.${ARCH}.json.xz
--- setup_all.sh
+++ setup_all.sh
@@ -8,12 +8,15 @@ mkdir kernels
 ./setup.sh arm
 ./setup.sh mipsel
 ./setup.sh mipseb
+./setup.sh mips64eb
 
 cp build/arm/arch/arm/boot/zImage kernels/zImage.armel
 cp build/arm/vmlinux kernels/vmlinux.armel
 
 cp build/mipseb/vmlinux kernels/vmlinux.mipseb
 cp build/mipsel/vmlinux kernels/vmlinux.mipsel
+
+cp build/mips64eb/vmlinux kernels/vmlinux.mips64eb
 
 PROF=kernels/firmadyne_profiles.conf
 echo "[armel]" > $PROF
@@ -22,6 +25,8 @@ echo "[mipseb]" >> $PROF
 cat panda_profile.mipseb >> $PROF
 echo "[mipsel]" >> $PROF
 cat panda_profile.mipsel >> $PROF
+echo "[mips64eb]" >> $PROF
+cat panda_profile.mips64eb >> $PROF
 
 mv ./vmlinux.arm.json.xz ./vmlinux.armel.json.xz
 cp ./vmlinux.*.json.xz kernels/
