c5a004c29e48c175b75b129647d90f2f2745e2f1
Cole DiLorenzo <coolkingcole@gmail.com>
2023-10-31 11:16:30 -0400

    para

--- docker_build.sh
+++ docker_build.sh
@@ -2,8 +2,13 @@
 #Commented out target list is what we'd like to have, but haven't tested
 #TARGETLIST="armel armeb mipseb mipsel mips64 mips64el aarch64"
 #These targets are ones we've tested
-TARGETLIST="armel mipseb mipsel mips64eb mips64el"
-
+ALL_TARGETS="armel mipseb mipsel mips64eb mips64el"
+if [ ! -z $1 ]
+then
+       TARGETLIST=$1
+else
+       TARGETLIST=ALL_TARGETS
+fi
 docker build -t igloo_kernel_builder . || exit
 docker run --rm -v `realpath .`:/linux  -w /linux -e IGLOO_TARGETLIST="$TARGETLIST" -e DOCKER_USER="$USER" \
        igloo_kernel_builder xonsh docker_inner_build_targets.xsh
