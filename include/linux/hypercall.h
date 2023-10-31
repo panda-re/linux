cb2f6558a7b65b36e28041acc1747a02b125573c
Andrew Fasano <fasano@mit.edu>
2023-10-31 12:24:07 -0400

    igloo_hypercall2: mips support

--- include/linux/hypercall.h
+++ include/linux/hypercall.h
@@ -31,12 +31,27 @@ static inline uint32_t igloo_hypercall2(uint32_t num, uint32_t a1, uint32_t a2) 
   asm volatile(
      "mcr p7, 0, r0, c0, c0, 0"
       : "+r"(r0)  // Input and output
-      : "r"(r1), "r"(r2)
+      : "r"(r1), "r"(r2) // Input only
       :  // Clobber list is empty because r0 is specified as an input-output operand
   );
 
   // Read the result from r0
   return r0;
+#elif defined(CONFIG_MIPS)
+    // MIPS specific code
+    register uint32_t a0 asm("a0") = num;  // Set up a0 with the value of num
+    register uint32_t a1 asm("a1") = a1;   // Argument 2
+    register uint32_t a2 asm("a2") = a2;   // Argument 3
+
+    asm volatile(
+       "movz $0, %[a0], %[a1]"
+        : "+r"(a0)  // Input and output
+        : [a1] "r" (a1), [a2] "r" (a2) // Input only
+        :  // Clobber list is empty because a0 is specified as an input-output operand
+    );
+
+    // Read the result from a0
+    return a0;
 #else
 #error "No igloo_hypercall2 support for architecture"
 #endif
