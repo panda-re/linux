6687a76dc914621efc9896d1c664eb69b07e034b
Andrew Fasano <fasano@mit.edu>
2023-10-31 13:43:42 -0400

    Dyndev bugfix for mips

--- include/linux/hypercall.h
+++ include/linux/hypercall.h
@@ -23,34 +23,32 @@ static inline void igloo_hypercall(uint32_t num, uint32_t a1) {
 #endif
 }
 
-static inline unsigned long igloo_hypercall2(unsigned long num, unsigned long a1, unsigned long a2) {
+static inline unsigned long igloo_hypercall2(unsigned long num, unsigned long arg1, unsigned long arg2) {
 #if defined(CONFIG_ARM)
     register unsigned long r0 asm("r0") = num;
-    register unsigned long r1 asm("r1") = a1;
-    register unsigned long r2 asm("r2") = a2;
+    register unsigned long r1 asm("r1") = arg1;
+    register unsigned long r2 asm("r2") = arg2;
 
     asm volatile(
        "mcr p7, 0, r0, c0, c0, 0"
         : "+r"(r0)  // Input and output
         : "r"(r1), "r"(r2)
-        :
+        : // No clobber
     );
 
     return r0;
 
 #elif defined(CONFIG_MIPS)
-    // MIPS specific code
     register unsigned long a0 asm("a0") = num;
-    register unsigned long a1 asm("a1") = a1;
-    register unsigned long a2 asm("a2") = a2;
+    register unsigned long a1 asm("a1") = arg1;
+    register unsigned long a2 asm("a2") = arg2;
 
     asm volatile(
-       "movz $0, %[a0], %[a1]"
-        : "+r"(a0)  // Input and output
-        : [a1] "r" (a1), [a2] "r" (a2)
-        :
+       "movz $0, $0, $0"
+        : "+r"(a0)  // Input and output in R0
+        : "r"(a1), "r"(a2) // arg1 in register A1, arg2 in register A2
+        : // No clobber
     );
-
     return a0;
 
 #else
