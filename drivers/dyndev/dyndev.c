0bbabdfa75b113c74e006d88c0a7ebc04cddb794
Andrew Fasano <fasano@mit.edu>
2023-11-14 11:07:39 -0500

    Dyndev: support dynamic mtd reads/writes with custom mtdpart hypercalls

--- drivers/dyndev/dyndev.c
+++ drivers/dyndev/dyndev.c
@@ -1,17 +1,25 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
 #include <asm/uaccess.h>
-#include <linux/slab.h>
 #include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/namei.h>
+#include <linux/slab.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+
 #include <linux/hypercall.h>
+#include <linux/dyndev.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrew");
 MODULE_DESCRIPTION("Dynamic devices");
 
-#define MAX_DEVICES 64  // Maximum number of devices
+bool hook_mtd=false; // Set by dyndev, checked by mtdpart
+EXPORT_SYMBOL(hook_mtd);
+
 static struct class*  my_class  = NULL; // The device-driver class struct pointer
 
 static char *devnames = "";
@@ -21,73 +29,6 @@ MODULE_PARM_DESC(devnames, "A comma-separated list of device names");
 static char **device_name;
 static int *device_major;
 static int num_devices = 0;
-
-#define HYPER_FILE_OP 0x100200
-enum request_type {
-    HYPER_READ,
-    HYPER_WRITE,
-    HYPER_IOCTL,
-    // Add other operation types as needed
-};
-
-struct hyper_read_args {
-    char *buffer;
-    size_t length;
-    loff_t offset;
-};
-
-struct hyper_write_args {
-    const char *buffer;
-    size_t length;
-    loff_t offset;
-};
-
-struct hyper_ioctl_args {
-    unsigned int cmd;
-    unsigned long arg;
-};
-
-struct hyper_file_op {
-    enum request_type type;
-    unsigned long rv;
-    char device_name[128];
-    union {
-        struct hyper_read_args read_args;
-        struct hyper_write_args write_args;
-        struct hyper_ioctl_args ioctl_args;
-    } args;
-};
-
-void sync_struct(struct hyper_file_op* struct_instance) {
-    int i;
-    volatile char junk = 0;
-    int max_tries = 100;
-    while (max_tries-- > 0) {
-        if (igloo_hypercall2(HYPER_FILE_OP, (unsigned long)struct_instance, (unsigned long)sizeof(struct hyper_file_op)) == 0)
-            break;
-        for (i = 0; i < sizeof(struct hyper_file_op); i++) {
-            // Ensure we read the entire structure just to make sure it's paged in
-            junk += ((char*)struct_instance)[i];
-        }
-
-        // Check if it's of type HYPER_READ and if so, copy the data back
-        if (struct_instance->type == HYPER_READ) {
-            // page in the buffer - read up to length bytes
-            for (i = 0; i < struct_instance->args.read_args.length; i++) {
-                junk += (struct_instance->args.read_args.buffer + struct_instance->args.read_args.offset)[i];
-            }
-        } else if (struct_instance->type == HYPER_WRITE) {
-            // page in the buffer - read up to length bytes
-            for (i = 0; i < struct_instance->args.write_args.length; i++) {
-                junk += (struct_instance->args.write_args.buffer + struct_instance->args.write_args.offset)[i];
-            }
-        }
-    }
-    (void)junk;  // Suppress possible unused variable warning
-    if (max_tries == 0) {
-        pr_emerg("dyndev: failed to sync struct\n");
-    }
-}
 
 static int dev_open(struct inode *inodep, struct file *filep) {
     return 0;
@@ -242,6 +183,14 @@ static int __init hyperdev_init(void) {
             continue;
         }
 
+        // If this token is 'mtd' we set a flag and skip
+        if (strncmp(token, "mtd", 3) == 0) {
+            // MTD is a special device that we handle with custom code in mtdpart.c
+            // We'll set the static bool in our header so it knows to do special stuff
+            hook_mtd = true;
+            continue;
+        }
+
         device_name[i] = kstrdup(token, GFP_KERNEL);
         // Initialize device_major[i] appropriately
         device_major[i] = register_chrdev(0, device_name[i], &fops);
@@ -255,6 +204,7 @@ static int __init hyperdev_init(void) {
         }
         i++;
     }
+
     return 0;
 }
 
@@ -271,9 +221,6 @@ static void __exit hyperdev_exit(void) {
 
     // Destroy the class
     class_destroy(my_class);
-
-
-
     kfree(device_major);
 }
 
