c5fab6634ff2437953f9cf89f81c95cef33fc428
Luke Craig <lacraig3@gmail.com>
2023-12-19 16:58:50 -0500

    lets just build 32-bit for the moment

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,7 @@
 #Commented out target list is what we'd like to have, but haven't tested
 #TARGETLIST="armel armeb mipseb mipsel mips64 mips64el aarch64"
 #These targets are ones we've tested
-TARGETLIST="armel mipseb mipsel mips64eb mips64el"
+TARGETLIST="armel mipseb mipsel"
 
 docker build -t igloo_kernel_builder . || exit
 docker run --rm -v `realpath .`:/linux  -w /linux -e IGLOO_TARGETLIST="$TARGETLIST" -e DOCKER_USER="$USER" \
