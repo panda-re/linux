4e34238b5bec415efeda03678058fb4dcec834e4
Luke Craig <lacraig3@gmail.com>
2024-01-10 13:31:27 -0500

    dyndev: get arguments from hypercall

--- drivers/dyndev/dyndev.c
+++ drivers/dyndev/dyndev.c
@@ -23,27 +23,49 @@
 #include "dyndev_devfs.h"
 #include "dyndev_procfs.h"
 #include "dyndev_netdev.h"
+#include "dyndev_sysfs.h"
+#include "hyperutils.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrew");
 MODULE_DESCRIPTION("Dynamic devices");
 
-static char *devnames = "";
-module_param(devnames, charp, 0000);
-MODULE_PARM_DESC(devnames, "A comma-separated list of device names");
+// static char *devnames = "";
+// module_param(devnames, charp, 0000);
+// MODULE_PARM_DESC(devnames, "A comma-separated list of device names");
 
-static char *procnames = "";
-module_param(procnames, charp, 0000);
-MODULE_PARM_DESC(procnames, "A comma-separated list of proc names");
+// static char *procnames = "";
+// module_param(procnames, charp, 0000);
+// MODULE_PARM_DESC(procnames, "A comma-separated list of proc names");
 
-static char *netdevnames = "";
-module_param(netdevnames, charp, 0000);
-MODULE_PARM_DESC(netdevnames, "A comma-separated list of network device names");
+// static char *netdevnames = "";
+// module_param(netdevnames, charp, 0000);
+// MODULE_PARM_DESC(netdevnames, "A comma-separated list of network device names");
 
+// static char *sysfs = "";
+// module_param(sysfs, charp, 0000);
+// MODULE_PARM_DESC(sysfs, "A comma-separated list of network device names");
+
+#define ARG_SIZE 4096
 
 static int __init hyperdev_init(void) {
     int rv;
     pr_emerg("dyndev: Initializing the dyndev module\n");
+    char *devnames = kmalloc(ARG_SIZE, GFP_KERNEL);
+    char *procnames = kmalloc(ARG_SIZE, GFP_KERNEL);
+    char *netdevnames = kmalloc(ARG_SIZE, GFP_KERNEL);
+    char *sysfs = kmalloc(ARG_SIZE, GFP_KERNEL);
+    loff_t offset;
+
+    hypervisor_read("dyndev.devnames", devnames, ARG_SIZE, &offset);
+    hypervisor_read("dyndev.procnames", procnames, ARG_SIZE, &offset);
+    hypervisor_read("dyndev.netdevnames", netdevnames, ARG_SIZE, &offset);
+    hypervisor_read("dyndev.sysfs", sysfs, ARG_SIZE, &offset);
+
+    printk(KERN_EMERG "dyndev: devnames: %s\n", devnames);
+    printk(KERN_EMERG "dyndev: procnames: %s\n", procnames);
+    printk(KERN_EMERG "dyndev: netdevnames: %s\n", netdevnames);
+    printk(KERN_EMERG "dyndev: sysfs: %s\n", sysfs);
 
     rv = dyndev_init_devfs(devnames);
     if (rv < 0) {
@@ -63,6 +85,12 @@ static int __init hyperdev_init(void) {
         return rv;
     }
 
+    rv = dyndev_init_sysfs(sysfs);
+    if (rv < 0) {
+        printk(KERN_ERR "dyndev: Failed to initialize sysfs\n");
+        return rv;
+    }
+
     printk(KERN_ERR "dyndev module loaded.\n");
     return 0;
 }
@@ -71,6 +99,7 @@ static void __exit hyperdev_exit(void) {
     dyndev_free_devfs();
     dyndev_free_procfs();
     dyndev_free_netdevs();
+    dyndev_free_sysfs();
     printk(KERN_ERR "dyndev module exited.\n");
 }
 
